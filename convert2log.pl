d9e72808a3107a1e297a570c6bbb68b63918eddc
Laura Connor <laura@orchid.site>
2019-10-07 13:12:24 -0400

    Updated documentation.

--- convert2log.pl
+++ convert2log.pl
@@ -1,7 +1,11 @@
+# convert2log.pl
+
+
 ######################################################################
-# This file is part of spatial_density.pl.
+#  This file convert2log.pl 
+#  is part of the spatial density package from Geoscience Community Codes 
 #
-#    spatial_density.pl is free software: you can redistribute it and/or modify
+#    convert2log.pl is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
@@ -13,9 +17,28 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with spatial_density.pl.
+
 #    If not, see <http://www.gnu.org/licenses/>.
+# 
+#    Copyright (C) 2014
+#    Laura Connor lconnor@usf.edu; ljconnor@gmail.com
 ######################################################################
 
+# Helper script for plot_spd.gmt.pl
+#
+# This routine has been updated so that it does not try to compute log of 0.
+# 
+# Also, a cutoff value is accepted on the commandline. The code will not
+# compute the log of any value below the cutoff value.
+#
+# Last updated: October, 2019
+###################################################################
+# 
+# This script prints out the log of each spatial density value > 0.
+# If a cutoff value is spcified on the commandline after the
+# file name then values less than the cuttof will be given the
+# value of the cutoff. This helps with smoothing out the gmt contouring
+# of very small values.
 my $cutoff = $ARGV[1];
 use File::Slurp;
 my @Lines = read_file("$ARGV[0]", chomp => 1); # will chomp() each line
@@ -25,5 +48,3 @@ foreach my $line (@Lines){
 	if ($log_sd < $cutoff) {$log_sd = $cutoff}
         my $text = sprintf("%0.2f", $log_sd);
 	print "$east $north $text\n";
-
-}
